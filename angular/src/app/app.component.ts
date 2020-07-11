7b0c4d0437b028b8e5734d2d57f2c254709060c4
salvador <hebustha@gmail.com>
2020-07-11 21:44:55 +0200

    a

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent implements OnInit{
     }
   }
   ngOnInit(): void {
-    console.log("a")
+    console.log("sqdsqda")
     if (this.post) {
       this.postService.setPost(this.post);
       if (this.isHome) {
